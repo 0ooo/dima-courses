5d53e89d1e6496cbd098ab2a101cb0b4fd6b7233
Pavel Gontarenko <pavgont@gmail.com>
2018-03-20 09:21:24 +0200

    First Lesson - simple Unit test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+
+class ExampleTest extends TestCase
+{
+	/** @test */
+	public function test_example()
+	{
+		$this->assertEquals(1, 1);
+	} 
+}
